cef84214b7ca2b0aa547c1201a0fecd5acf1f6e6
Tori <aer456987@gmail.com>
2021-08-30 22:11:03 +0800

    js 程式碼拆分與優化

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,58 +1,45 @@
-$(function() {
-  AOS.init({
-    offset: 50,
-    once: true,
+
+AOS.init({
+  offset: 50,
+  once: true,
+});
+
+const msgBtn = document.querySelector('.js-msg-submit');
+if (msgBtn) {
+  const msgFromName = document.querySelector('#userName');
+  const msgFromEmail = document.querySelector('#userEmail');
+  const msgFromPhone = document.querySelector('#userPhone');
+  const msgFromMsg = document.querySelector('#userMassage');
+  chackMsgFromValue();
+
+  $('.js-msg-form').on('change', function() {
+    chackMsgFromValue();
   });
 
-  const msgBtn = document.querySelector('.js-msg-submit');
-  if (msgBtn) {
-    const msgFromName = document.querySelector('#userName');
-    const msgFromEmail = document.querySelector('#userEmail');
-    const msgFromPhone = document.querySelector('#userPhone');
-    const msgFromMsg = document.querySelector('#userMassage');
-    chackMsgFromValue();
-
-    $('.js-msg-form').on('change', function() {
-      chackMsgFromValue();
-    });
-
-    function chackMsgFromValue() {
-      if(
-        msgFromName.value === ''
-        || msgFromEmail.value === ''
-        || msgFromPhone.value === ''
-        || msgFromMsg.value === ''
-      ) {
-        $(msgBtn).addClass('btn-outline-primary-light');
-        $(msgBtn).prop({ disabled: true });
-      } else {
-        $(msgBtn).removeClass('btn-outline-primary-light');
-        $(msgBtn).prop({ disabled: false });
-      }
+  function chackMsgFromValue() {
+    if(
+      msgFromName.value === ''
+      || msgFromEmail.value === ''
+      || msgFromPhone.value === ''
+      || msgFromMsg.value === ''
+    ) {
+      $(msgBtn).addClass('btn-outline-primary-light');
+      $(msgBtn).prop({ disabled: true });
+    } else {
+      $(msgBtn).removeClass('btn-outline-primary-light');
+      $(msgBtn).prop({ disabled: false });
     }
   }
+}
 
 
-  const dateDom = document.querySelector('.js-datepicker-date');
-  if (dateDom) {
-    const datepicker = new Datepicker(dateDom, {
-      autohide: true,
-      daysOfWeekDisabled: [0],
-      nextArrow: '>',
-      prevArrow: '<',
-      buttonClass: 'btn btn-outline-primary border-0 fw-normal',
-    });
-  }
-
-
-  const openModalBtn = document.querySelectorAll('.js-course-modal-btn');
-  if(openModalBtn) {
-    const courseModal = new bootstrap.Modal(document.querySelector('.js-course-modal'));
-
-    openModalBtn.forEach(function(item) {
-      $(item).on('click', function() {
-        courseModal.show();
-      });
-    });
-  }
-});
+const dateDom = document.querySelector('.js-datepicker-date');
+if (dateDom) {
+  const datepicker = new Datepicker(dateDom, {
+    autohide: true,
+    daysOfWeekDisabled: [0],
+    nextArrow: '>',
+    prevArrow: '<',
+    buttonClass: 'btn btn-outline-primary border-0 fw-normal',
+  });
+}
--- app/assets/js/modal.js
+++ app/assets/js/modal.js
@@ -1,23 +1,21 @@
-let modelImg = '';
-let modelTitle = '';
-let modelTeacher = '';
+const openModalBtn = document.querySelectorAll('.js-course-modal-btn');
+if(openModalBtn) {
+  const courseModal = new bootstrap.Modal(document.querySelector('.js-course-modal'));
 
-
-
+  openModalBtn.forEach(function(item) {
+    $(item).on('click', function() {
+      courseModal.show();
+    });
+  });
+}
 
 
 $('.js-course-data').on('click', function() {
-  modelImg = $(this).find('img').attr('src');
-  modelTitle = $(this).find('h3').text();
-  modelTeacher = $(this).find('p').find('span').text();
-  // const dataImg = $(this).find('img').attr('src');
-  // const dataTitle = $(this).find('h3').text();
-  // const dataTeacher = $(this).find('p').find('span').text();
+  let modelImg = $(this).find('img').attr('src');
+  let modelTitle = $(this).find('h3').text();
+  let modelTeacher = $(this).find('p').find('span').text();
 
   $('.js-model-img').attr('src', modelImg);
   $('.js-model-title').text(modelTitle);
   $('.js-model-teacher').text(modelTeacher);
-
-
-
 });
