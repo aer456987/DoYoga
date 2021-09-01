0534ad4aee2d838ae3a28f1c494b70cf88bad0d7
Tori <aer456987@gmail.com>
2021-09-02 00:58:09 +0800

    Update 2021-09-01T16:58:06.873Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -60,8 +60,8 @@ var formRules = {
     msg: '不能有特殊符號'
   },
   age: {
-    rule: /^\d$/,
-    msg: '只能是數字'
+    rule: /^\d{2}$/,
+    msg: '只能是 2 碼數字'
   },
   email: {
     rule: /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/,
@@ -173,6 +173,7 @@ function getReserveDate() {
   inputData.userData.gender = $('#reserveGender').val();
   inputData.userData.email = $('#reserveEmail').val();
   inputData.userData.phone = $('#reservePhone').val();
+  inputData.userData.age = $('#reserveAge').val().parseInt();
   return inputData;
 }
 
